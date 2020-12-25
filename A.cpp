6a1a46cf4fef771723901b4bd4b744a2e4a2853d
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-25 17:37:18 +0800

    Create A.cpp

---
+++ A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n, w;
+	cin >> n >> w;
+  //just divide the total weight that the truck can carry by the weight of each brick
+	cout << n/w << "\n";
+}
